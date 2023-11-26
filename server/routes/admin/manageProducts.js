752e11dd48dc070f70e36519fc191f36fee86682
Parimal07 <parimalkolhe.pk@gmail.com>
2023-11-26 10:47:12 +0530

    Updated Product Logic

--- server/routes/admin/manageProducts.js
+++ server/routes/admin/manageProducts.js
@@ -4,18 +4,18 @@ import Product from "../../models/product.js";
 const router = Router();
 
 // Route to get all products for admin management
-// router.get('/manageproducts', async (req, res) => {
-//   try {
-//     const products = await Product.find(); // Retrieve all products
-//     res.json(products);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-router.get("/manageproducts", (req, res) => {
-  // Handle the GET request here
-  res.send("This is the manageproduct route");
+router.get('/manageproducts', async (req, res) => {
+  try {
+    const products = await Product.find(); // Retrieve all products
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+// router.get("/manageproducts", (req, res) => {
+//   // Handle the GET request here
+//   res.send("This is the manageproduct route");
+// });
 
 export default router;
