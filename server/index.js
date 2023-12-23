e69267a09c634fe9ecc67c8243135518da0a9895
Viren Khatri <vk102002@gmail.com>
2023-12-23 12:10:44 +0530

    vercel config

--- server/index.js
+++ server/index.js
@@ -71,12 +71,12 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: process.env.GENERAL_SECRET_CODE,
+    secret: "secretcode",
     resave: true,
     saveUninitialized: true,
   })
 );
-app.use(cookieParser(process.env.GENERAL_SECRET_CODE));
+app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
 app.use(passport.session());
 passportConfig(passport);
